9af728381cd24846f66fec785f60f44425f4c8f9
Chris Carlson <ccarlson@idsinternational.com>
2021-08-26 00:03:58 -0400

    cleanup

--- www.terrycjennings.com/~globals/footer.js
+++
@@ -1,6 +0,0 @@
-// footer.js
-// stores the name to put into the footer copyright
-var addCopyright = true;
-var footerName = "Terry Catasus Jennings";
-if (addCopyright)
-document.write(" by " + footerName + " &#169; 2020 ");
